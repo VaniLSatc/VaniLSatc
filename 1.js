2b20fe5d1b840873884b977298dae960ef981fa0
VaniLSatc <iwan.ivan432@yandex.ru>
2025-04-27 18:00:47 +0700

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,36 @@
+function addSegment(main, domain, ibegin, iend) {
+    if (ibegin > 0 && main[ibegin - 1] === '1') {
+        domain += main[ibegin - 1];
+    }
+    for (let i = ibegin; i <= iend; i++) {
+        if (i < main.length) {
+            domain += main[i];
+        }
+    }
+    return domain + ", ";
+}
+
+function main() {
+    const str1 = prompt("Введите строку:") || "";
+    let str2 = "";
+    
+    for (let i = 1; i < str1.length; i++) {
+        if (str1[i] === '0' && str1[i - 1] === '1') {
+            let iend = i;
+            while (iend < str1.length && str1[iend] === '0') {
+                iend++;
+            }
+            if (iend < str1.length && str1[iend] === '1') {
+                str2 = addSegment(str1, str2, i - 1, iend);
+            }
+        }
+    }
+    
+    if (str2.length > 0) {
+        str2 = str2.slice(0, -2);
+    }
+    
+    console.log(str2);
+}
+
+main();
